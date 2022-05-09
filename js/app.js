474655218d6a389e40ad3b30da5eb60a9d8d7034
Dairon Camargo Granada <93607018+dairongranada@users.noreply.github.com>
2022-05-09 15:46:17 -0500

    Aplicacion Web (Style Tatto) 2.1 Version.DG

---
+++ js/app.js
@@ -0,0 +1,17 @@
+
+//       M E N U   H A M B U R G U E S A 
+
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-100%';
+}
